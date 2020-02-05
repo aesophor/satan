77d3e441ca6e9da74ff01f65046001ccc873e625
Marco Wang <m.aesophor@gmail.com>
2020-02-05 07:57:30 -0500

    remove hide_file.{c,h}

--- hide_file.c
+++
@@ -1,2 +0,0 @@
-// Copyright (c) 2020 Marco Wang <m.aesophor@gmail.com>
-
--- hide_file.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2020 Marco Wang <m.aesophor@gmail.com>
-#ifndef SATAN_HIDE_FILE_H_
-#define SATAN_HIDE_FILE_H_
-
-void satan_hide_file(const char *filename);
-
-#endif  // SATAN_HIDE_FILE_H_
